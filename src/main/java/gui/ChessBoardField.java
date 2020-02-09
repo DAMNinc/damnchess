0b777848b28299b59c74aad6e5a0e74b814ae3ea
Andreas Schmidt Jensen <andreas@nivo.nu>
2020-02-09 16:17:19 +0100

    Changed encoding to ISO-8859-11

--- src/main/java/gui/ChessBoardField.java
+++ src/main/java/gui/ChessBoardField.java
@@ -1,17 +1,10 @@
 package gui;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
-
-import javax.swing.JPanel;
-
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import common.Datalink;
-import common.Vars;
-
 import engine.Piece;
+
+import javax.swing.*;
+import java.awt.*;
 
 
 public class ChessBoardField extends JPanel
